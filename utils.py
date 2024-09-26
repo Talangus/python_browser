d731517b8828d8954f3f3f07b19c6c3ffe2cd28c
Tal <tal.langus@ibm.com>
2024-09-26 11:28:55 +0300

    new view source parser

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ class Text:
         self.text = text
         self.children = []
         self.parent = parent
+        self.is_html_element_text = False
 
     def __repr__(self):
         return repr(self.text)
