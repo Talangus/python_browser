06607fc1f33c73cde440110bcd70b25a2198cc4b
Tal <tal.langus@ibm.com>
2024-11-22 12:11:01 +0200

    fix layout bugs

--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ import os
 import re
 from datetime import datetime, timedelta
 from pathlib import Path
-from element import Element
-# from document_layout import DocumentLayout
 
 class CustomError(Exception):
     pass
